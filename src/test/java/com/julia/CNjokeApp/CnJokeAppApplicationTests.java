bb7eec60914568b0ea2ed51ad467bfbc782d9752
julia lopez <julia.lopez@globant.com>
2022-01-31 17:32:26 -0300

    maven project setup

---
+++ src/test/java/com/julia/CNjokeApp/CnJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julia.CNjokeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
