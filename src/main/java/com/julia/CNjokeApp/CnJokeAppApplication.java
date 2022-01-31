8468f2e6f0e4eb36de207fac13c6b6e7e7bebb78
julia lopez <julia.lopez@globant.com>
2022-01-31 17:42:38 -0300

    Setting up maven project

---
+++ src/main/java/com/julia/CNjokeApp/CnJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.julia.CNjokeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnJokeAppApplication.class, args);
+	}
+
+}
